ce2cd9d29d65b45ec26db1e8e28a8ca9cc8e0ff3
eflorez09 <eflorez09@gmail.com>
2019-11-01 19:29:18 -0500

    agregar MaxConcurrentConnections

--- B2C/MoteeQueso.Api/MoteeQueso.Api/Program.cs
+++ B2C/MoteeQueso.Api/MoteeQueso.Api/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MoteeQueso.Api
 {
@@ -18,9 +14,14 @@ namespace MoteeQueso.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        .ConfigureServices((context, services) =>
+        {
+            services.Configure<KestrelServerOptions>(
+                context.Configuration.GetSection("Kestrel"));
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
     }
 }
--- B2C/MoteeQueso.Api/MoteeQueso.Api/Startup.cs
+++ B2C/MoteeQueso.Api/MoteeQueso.Api/Startup.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using MoteeQueso.Infraestructure.Data;
 
 namespace MoteeQueso.Api
 {
@@ -29,6 +21,9 @@ namespace MoteeQueso.Api
         {
             services.AddCors();
             services.AddControllers();
+
+            services.Configure<KestrelServerOptions>(
+        Configuration.GetSection("Kestrel"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
