1429e0bb9257ab8d1ddb5dcca6e6b1cee6fafeea
GioLopez <giovanni.lopez@cwc.com>
2019-11-11 15:59:21 -0500

    Added product by code

--- OMS/productos/src/main/java/com/toures/balon/productos/MainController.java
+++ OMS/productos/src/main/java/com/toures/balon/productos/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 @Controller
 @RequestMapping(path="/producto")
 public class MainController {
--- OMS/productos/src/main/java/com/toures/balon/productos/ProductDetailRepository.java
+++ OMS/productos/src/main/java/com/toures/balon/productos/ProductDetailRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface ProductDetailRepository extends PagingAndSortingRepository<ProductDetail, Long> {
 
-    @Query(value = "SELECT codigo, nombre, url_imagen FROM producto p WHERE p.codigo = :productCode",
+    @Query(value = "SELECT * FROM producto p WHERE p.codigo = :productCode",
             nativeQuery = true)
-    Collection findProudctByCode(@Param("productCode") String codigo);
+    ProductDetail findProudctByCode(@Param("productCode") String codigo);
+
 }
