84c8f5d00408e2a604814033c637da7bfe85d6f4
eflorez09 <eflorez09@gmail.com>
2019-11-22 16:05:51 -0500

    servicio AmericanAirLines

---
+++ PROVIDERS/AmericanAirlines/Api/Controllers/FlightController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AAFlightsService.ViewModels;
+
+namespace AAFlightsService.Api.Controllers
+{
+    //[EnableCors("AllowMyOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private static bool success = false;
+
+        [HttpPost]
+        [Route("reserveFlight")]
+        public IActionResult reserveFlight(FlightViewModel flightViewModel)
+        {
+            success = !success;
+
+            if (success)
+            {
+                return Ok(Guid.NewGuid());
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("cancelFlight")]
+        public IActionResult cancelFlight(FlightViewModel flightViewModel)
+        {
+            success = !success;
+
+            if (success)
+            {
+                return Ok(Guid.NewGuid());
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ PROVIDERS/AmericanAirlines/Api/ViewModels/FlightViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AAFlightsService.ViewModels
+{
+    public class FlightViewModel
+    {
+        public int id { get; set; }
+
+        public int agreement_id { get; set; }
+
+        public int tickets { get; set; }
+
+        public Guid filed { get; set; }
+    }
+}
