03e8963943919e9ae606cde9d406cd0a633c77b5
yesid gutierrez <ygutierrezd@outlook.com>
2019-11-23 00:43:40 -0500

    Bolivariano

---
+++ PROVIDERS/Bolivariano/Api/Controllers/BolivarianoController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BolivarianoController : ControllerBase
+    {
+        public bool success = false;
+
+        [HttpPost]
+        [Route("reservarPasaje")]
+        public IActionResult reservarPasaje(TransporteViewModel transporte)
+        {
+            success = !success;
+
+            if (success)
+            {
+                return Ok(WriteToFile(transporte));
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("cancelarTransporte")]
+        public IActionResult cancelarTransporte(TransporteViewModel transporte)
+        {
+            success = !success;
+
+            if (success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        private string WriteToFile(TransporteViewModel Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\ArchivosCompartidos";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\ArchivosCompartidos\\touresbalon_orden_" + Message.orderId + ".csv";
+            //if ()
+            //{
+            //    // Create a file to write to.   
+            //    using (StreamWriter sw = File.CreateText(filepath))
+            //    {
+            //        sw.WriteLine(Message);
+            //    }
+            //}
+            //else
+            //{
+            //    using (StreamWriter sw = File.AppendText(filepath))
+            //    {
+            //        sw.WriteLine(Message);
+            //    }
+            //}
+            return filepath;
+        }
+    }
+}
---
+++ PROVIDERS/Bolivariano/Api/Models/TransporteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class TransporteViewModel
+    {
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+        public string fecha_salida { get; set; }
+        public string num_viaje { get; set; }
+        public string num_silla { get; set; }
+        public string ciudad_origen { get; set; }
+        public string ciudad_destino { get; set; }
+        public string hora_salida { get; set; }
+        public string estado_reserva { get; set; }
+        public string orderId { get; set; }
+    }
+}
