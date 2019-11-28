c89e5fe99670c75c6864d399ae18fc3b9409183a
yesid gutierrez <ygutierrezd@outlook.com>
2019-11-28 18:55:25 -0500

    consumer Orders

---
+++ Consumers_RabbitMQ/ReciveOrders/Modelos/OrderDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReciveOrders.Modelos
+{
+    class OrderDetailViewModel
+    {
+        public OrderDetailViewModel()
+        {
+            items = new List<ItemViewModel>();
+        }
+
+        public int id { get; set; }
+
+        public int customer_id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public decimal price { get; set; }
+
+        public int status_id { get; set; }
+
+        public string comments { get; set; }
+
+        public List<ItemViewModel> items { get; set; }
+    }
+}
