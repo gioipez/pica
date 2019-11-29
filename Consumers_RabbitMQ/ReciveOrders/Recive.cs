4952e86269476e42778858dc6db1732598a74883
yesid gutierrez <ygutierrezd@outlook.com>
2019-11-28 19:14:13 -0500

    ajuste mensajes

--- Consumers_RabbitMQ/ReciveOrders/Recive.cs
+++ Consumers_RabbitMQ/ReciveOrders/Recive.cs
@@ -47,18 +47,18 @@ namespace ReciveOrders
                     OrderDetailViewModel order = JsonConvert.DeserializeObject<OrderDetailViewModel>(message);
 
                     ClienteRestSharp cliente = new ClienteRestSharp();
-                    IRestResponse responseCustomer = cliente.Request(urlCreateOrder, Method.POST, order);
+                    IRestResponse responseOrder = cliente.Request(urlCreateOrder, Method.POST, order);
                     //IRestResponse responseAuth = cliente.Request(urlCreateUsrAuth, Method.POST, userAuth);
 
-                    if (responseCustomer.StatusCode == HttpStatusCode.OK)
+                    if (responseOrder.StatusCode == HttpStatusCode.OK)
                     {
                         channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                        Console.WriteLine("Ok Response Customer -> {0}", responseCustomer.Content);
+                        Console.WriteLine("Ok Response Order -> {0}", responseOrder.Content);
                     }
                     else
                     {
                         Console.WriteLine("Fail");
-                        Console.WriteLine(" Customer StatusCode -> {0} Response -> {1}", responseCustomer.StatusCode, responseCustomer.Content);
+                        Console.WriteLine(" Response StatusCode -> {0} Response -> {1}", responseOrder.StatusCode, responseOrder.Content);
 
                         channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 
